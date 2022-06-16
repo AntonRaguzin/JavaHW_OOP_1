9cca2eae34474f1aaae3811343107b4c5f07f7be
antonraguzin <raguzin82@gmail.com>
2022-06-16 12:58:00 +0300

    Переменные заполняются

---
+++ src/main/java/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+    public static void main(String[] args) {
+        Post post = new Post();
+
+        post.birthday.day = 15;
+        post.birthday.month = 6;
+        post.birthday.year = 1980;
+
+        post.name = "Иван";
+        post.passport = "3333 № 111222";
+        post.patronymic = "Иванович";
+        post.phone = "+7 (999)-999-99-99";
+        post.surname = "Иванов";
+        post.subscription = true;
+
+    }
+
+}
